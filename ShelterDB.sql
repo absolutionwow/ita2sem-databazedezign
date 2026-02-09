ee4d535db19f89a2858ef2ac6c4e185513c355a5
absolutionwow <andreas.dahlp@gmail.com>
2026-02-09 11:42:38 +0100

    Update ShelterDB.sql

--- ShelterDB.sql
+++ ShelterDB.sql
@@ -2,51 +2,60 @@ CREATE DATABASE ShelterDB
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    CREATE TABLE Bruger (
-        BrugerID INT PRIMARY KEY,
-        BrugerNavn TEXT,
-        Email TEXT
-    )
+CREATE TABLE Bruger (
+    BrugerID INT PRIMARY KEY,
+    BrugerNavn TEXT,
+    Email TEXT
+);
 
-    CREATE TABLE Lokalitet (
-        LoktalitetID INT PRIMARY KEY,
-        LokatalitetNavn TEXT
-    )
+CREATE TABLE Lokalitet (
+    LokalitetID INT PRIMARY KEY,
+    LokalitetNavn TEXT
+);
 
-    CREATE TABLE Shelter (
-        ShelterID INT PRIMARY KEY,
-        ShelterNavn TEXT,
-        Areal INT,
-        Billede TEXT,
-        LokalitetID INT FOREIGN KEY
-    )
+CREATE TABLE Shelter (
+    ShelterID INT PRIMARY KEY,
+    ShelterNavn TEXT,
+    Areal INT,
+    Billede TEXT,
+    LokalitetID INT,
+    FOREIGN KEY (LokalitetID) REFERENCES Lokalitet(LokalitetID)
+);
 
-    CREATE TABLE Booking (
-        BookingID INT PRIMARY KEY,
-        BrugerID INT FOREIGN KEY,
-        ShelterID INT FOREIGN KEY,
-        Fra DATE,
-        Til DATE
-    )
+CREATE TABLE Booking (
+    BookingID INT PRIMARY KEY,
+    BrugerID INT,
+    ShelterID INT,
+    Fra DATE,
+    Til DATE,
+    FOREIGN KEY (BrugerID) REFERENCES Bruger(BrugerID),
+    FOREIGN KEY (ShelterID) REFERENCES Shelter(ShelterID)
+);
 
-    INSERT INTO Bruger VALUES (101, "Jens", "jens@gmail.com");
-    INSERT INTO Bruger VALUES (102, "Mette", "mette@gmail.com");
-    INSERT INTO Bruger VALUES (103, "Henrik", "henrik@gmail.com");
 
-    INSERT INTO Lokalitet VALUES (1, "Skovhytte");
-    INSERT INTO Lokalitet VALUES (2, "Strandhytte");
-    INSERT INTO Lokalitet VALUES (3, "Bjerghytte");
+INSERT INTO Bruger VALUES
+(101, 'Jens', 'jens@gmail.com'),
+(102, 'Mette', 'mette@gmail.com'),
+(103, 'Henrik', 'henrik@gmail.com');
 
-    INSERT INTO Shelter VALUES (1, "Shelter A", 30, "images/a.png", 1);
-    INSERT INTO Shelter VALUES (2, "Shelter B", 40, "images/b.png", 1);
-    INSERT INTO Shelter VALUES (3, "Shelter C", 25, "images/c.png", 2);
-    INSERT INTO Shelter VALUES (4, "Shelter D", 50, "images/d.png", 3);
-    INSERT INTO Shelter VALUES (5, "Shelter E", 45, "images/e.png", 3);
+INSERT INTO Lokalitet VALUES
+(1, 'Skovhytte'),
+(2, 'Strandhytte'),
+(3, 'Bjerghytte');
 
-    INSERT INTO Booking VALUES (1, 101, 1, 2025-03-15, 2025-03-16);
-    INSERT INTO Booking VALUES (1, 101, 2, 2025-03-15, 2025-03-16);
-    INSERT INTO Booking VALUES (2, 102, 3, 2025-03-16, 2025-03-17);
-    INSERT INTO Booking VALUES (3, 103, 4, 2025-03-17, 2025-03-18);
-    INSERT INTO Booking VALUES (3, 103, 5, 2025-03-17, 2025-03-18);
+INSERT INTO Shelter VALUES
+(1, 'Shelter A', 30, 'images/a.png', 1),
+(2, 'Shelter B', 40, 'images/b.png', 1),
+(3, 'Shelter C', 25, 'images/c.png', 2),
+(4, 'Shelter D', 50, 'images/d.png', 3),
+(5, 'Shelter E', 45, 'images/e.png', 3);
+
+INSERT INTO Booking VALUES
+(1, 101, 1, '2025-03-15', '2025-03-16'),
+(2, 101, 2, '2025-03-15', '2025-03-16'),
+(3, 102, 3, '2025-03-16', '2025-03-17'),
+(4, 103, 4, '2025-03-17', '2025-03-18'),
+(5, 103, 5, '2025-03-17', '2025-03-18');
+
 
 
